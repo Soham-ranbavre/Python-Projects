be6d42a3f642609a7b0b38fc6aa523aac3a1d94b
Soham Adinath Ranbavre <sohamranbavre587@gmail.com>
2026-08-14 21:15:39 +0530

    Day 7: Practice tuples and sets

---
+++ tuples_sets.py
@@ -0,0 +1,13 @@
+# Day 7 - Tuples and Sets
+
+# Tuple
+languages = ("Python", "C++", "SQL")
+
+print("Languages:", languages)
+print("First language:", languages[0])
+
+# Set
+skills = {"Python", "SQL", "Python", "C++"}
+
+print("Skills:", skills)
+print("Number of skills:", len(skills))
