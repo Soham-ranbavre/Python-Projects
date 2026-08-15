232dfc34b6b80f2f16f6d78a650a866460d0ec6a
Soham Adinath Ranbavre <sohamranbavre587@gmail.com>
2026-08-15 10:50:09 +0530

    Add student profile program

---
+++ student_profile.py
@@ -0,0 +1,17 @@
+# Student Profile
+
+student = {
+    "name": "Student",
+    "age": 21,
+    "branch": "AIML",
+    "cgpa": 7.5
+}
+
+student["cgpa"] = 7.8
+student["city"] = "Pune"
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Branch:", student["branch"])
+print("CGPA:", student["cgpa"])
+print("City:", student["city"])
