27219efad8c0c2e77dac2190498c9f95484148e5
Soham Adinath Ranbavre <sohamranbavre587@gmail.com>
2026-08-15 10:48:11 +0530

    Add basic calculator program

---
+++ basic_calculator.py
@@ -0,0 +1,9 @@
+# Basic Calculator
+
+n1 = int(input("Enter your number: "))
+n2 = int(input("Enter your number: "))
+
+print("Addition:", n1 + n2)
+print("Subtraction:", n1 - n2)
+print("Multiplication:", n1 * n2)
+print("Division:", n1 / n2)
